708999ea30393b314be367488c9cd80698f9dea4
nandakumarreddy-anr <nandakumarreddy63@gmail.com>
2026-06-03 09:26:04 +0530

    Handle DB connection errors

--- app.py
+++ app.py
@@ -18,20 +18,20 @@ ADMIN_EMAIL = "nandakumarreddy63@gmail.com"
 
 # ---------------- DATABASE ----------------
 
-print("DB_HOST =", os.getenv("DB_HOST"))
-print("DB_PORT =", os.getenv("DB_PORT"))
-print("DB_USER =", os.getenv("DB_USER"))
-print("DB_NAME =", os.getenv("DB_NAME"))
+try:
+    db = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
+    cursor = db.cursor(buffered=True)
 
-db = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    port=int(os.getenv("DB_PORT")),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME"),
-    ssl_disabled=False
-)
-cursor = db.cursor(buffered=True)
+except Exception as e:
+    print("DATABASE ERROR:", e)
+    db = None
+    cursor = None
 # ---------------- LOAD MODELS ----------------
 try:
     sms_model = joblib.load("sms_model.pkl")
